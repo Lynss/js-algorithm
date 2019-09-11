837cc3657f03b08a5b0082d85d5145ec18e19a20
longyun <ly1169134156@163.com>
2019-09-11 10:00:03 +0800

    self dividing numbers

---
+++ src/self-dividing-numbers.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+
+/**
+ * https://leetcode.com/problems/self-dividing-numbers/
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function (left, right) {
+    return _.range(left, right + 1).filter(number => number.toString().split('').every(d => number % d === 0));
+};
+
+console.assert(selfDividingNumbers(1, 22), [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]);
